f4ca30948115c4ead1b49c162b3fad0ca6ae9088
tmaioli <tmaioli@deloitte.it>
2023-10-23 00:21:49 +0200

    fix: error in types

--- src/server/api/routers/s3.ts
+++ src/server/api/routers/s3.ts
@@ -2,9 +2,8 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "../trpc";
 import { env } from "@/env.mjs";
-import { PutObjectCommand, UploadPartCommand } from "@aws-sdk/client-s3";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { TRPCError } from "@trpc/server";
 
 export const s3Router = createTRPCRouter({
   getObjects: publicProcedure.query(async ({ ctx }) => {
--- src/server/helpers/ssgHelper.ts
+++ src/server/helpers/ssgHelper.ts
@@ -2,10 +2,11 @@ import { createServerSideHelpers } from "@trpc/react-query/server";
 import { appRouter } from "@/server/api/root";
 import { prisma } from "@/server/db";
 import SuperJSON from "superjson";
+import { S3 } from "@aws-sdk/client-s3";
 
 export const generateSSGHelper = () =>
   createServerSideHelpers({
     router: appRouter,
-    ctx: { prisma, userId: null },
+    ctx: { prisma, userId: null, s3: new S3 },
     transformer: SuperJSON,
   });
